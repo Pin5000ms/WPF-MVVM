4710e49e8256ddfa461336757dba08c3027a1b21
Tom/TY.Chang <Tom.TY.Chang@deltaww.com>
2022-11-01 09:03:31 +0800

    11/1

---
+++ ModernDesign/ModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using ModernDesign.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernDesign.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        private object _currentView;
+        public object CurrentView 
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            } 
+        }
+
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+
+            HomeViewCommand = new RelayCommand(o => { CurrentView = HomeVM; });
+            DiscoveryViewCommand = new RelayCommand(o => { CurrentView = DiscoveryVM; });
+
+            CurrentView = HomeVM;
+        }
+    }
+}
