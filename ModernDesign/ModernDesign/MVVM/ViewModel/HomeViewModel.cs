d6e1006d58371832297dab5c9e915b98f9267eec
Tom/TY.Chang <Tom.TY.Chang@deltaww.com>
2022-11-01 16:53:13 +0800

    Dynamic block

--- ModernDesign/ModernDesign/MVVM/ViewModel/HomeViewModel.cs
+++ ModernDesign/ModernDesign/MVVM/ViewModel/HomeViewModel.cs
@@ -8,5 +8,36 @@ namespace ModernDesign.MVVM.ViewModel
 {
     class HomeViewModel
     {
+        public HomeViewModel()
+        {
+            _homePageBlocks.Add(new Block("A", 400, 200));
+            _homePageBlocks.Add(new Block("B", 200, 200));
+            _homePageBlocks.Add(new Block("C", 200, 200));
+        }
+
+        private List<Block> _homePageBlocks = new List<Block>();
+
+        public List<Block> HomePageBlocks
+        {
+            get { return _homePageBlocks; }
+            set { _homePageBlocks = value; }
+        }
+        
+
     }
+    public class Block
+    {
+        public int blockW { get; set; }
+        public int blockH { get; set; }
+        public string Name { get; set; }
+        public Block(string name, int w, int h)
+        {
+            Name = name;
+            blockW = w;
+            blockH = h;
+        }
+    }
+
+
+
 }
