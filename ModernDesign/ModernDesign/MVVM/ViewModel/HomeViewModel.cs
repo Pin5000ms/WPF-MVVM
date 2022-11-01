04a1ae35aea3601d1b3018bdbc95dc7a25ea2d0a
Tom/TY.Chang <Tom.TY.Chang@deltaww.com>
2022-11-01 17:11:27 +0800

    Color

--- ModernDesign/ModernDesign/MVVM/ViewModel/HomeViewModel.cs
+++ ModernDesign/ModernDesign/MVVM/ViewModel/HomeViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace ModernDesign.MVVM.ViewModel
 {
@@ -10,9 +11,9 @@ namespace ModernDesign.MVVM.ViewModel
     {
         public HomeViewModel()
         {
-            _homePageBlocks.Add(new Block("A", 400, 200));
-            _homePageBlocks.Add(new Block("B", 200, 200));
-            _homePageBlocks.Add(new Block("C", 200, 200));
+            _homePageBlocks.Add(new Block("A", 400, 200, Colors.BlueViolet));
+            _homePageBlocks.Add(new Block("B", 200, 200, Colors.Gold));
+            _homePageBlocks.Add(new Block("C", 200, 200, Colors.DarkGray));
         }
 
         private List<Block> _homePageBlocks = new List<Block>();
@@ -30,11 +31,14 @@ namespace ModernDesign.MVVM.ViewModel
         public int blockW { get; set; }
         public int blockH { get; set; }
         public string Name { get; set; }
-        public Block(string name, int w, int h)
+
+        public Color blockColor { get; set; }
+        public Block(string name, int w, int h, Color c)
         {
             Name = name;
             blockW = w;
             blockH = h;
+            blockColor = c;
         }
     }
 
