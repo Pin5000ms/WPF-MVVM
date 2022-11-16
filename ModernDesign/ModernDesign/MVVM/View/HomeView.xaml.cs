dfe971379785ef11bf3bf03b443575c103fece59
Tom/TY.Chang <Tom.TY.Chang@deltaww.com>
2022-11-16 17:18:38 +0800

    drag drop

--- ModernDesign/ModernDesign/MVVM/View/HomeView.xaml.cs
+++ ModernDesign/ModernDesign/MVVM/View/HomeView.xaml.cs
@@ -24,11 +24,19 @@ namespace ModernDesign.MVVM.View
     public partial class HomeView : UserControl
     {
         private HomeViewModel HomeVM;
+
+        private bool _isMoving;
+        private Point? _buttonPosition;
+        private double deltaX;
+        private double deltaY;
+        private TranslateTransform _currentTT;
+
         public HomeView(HomeViewModel _homeVM)
         {
             InitializeComponent();
             DataContext = _homeVM;
             HomeVM = _homeVM;
+            CompositionTarget.Rendering += OnRendering;
         }
         private void Drop(object sender, DragEventArgs e)
         {
@@ -50,25 +58,84 @@ namespace ModernDesign.MVVM.View
             }
         }
 
+        private void DragLeave(object sender, DragEventArgs e)
+        {
+
+        }
+
         private void PreviewMouseMove(object sender, MouseEventArgs e)
         {
+            if (_isMoving)
+            {
+                var mousePoint = Mouse.GetPosition(blocklistview);
+
+                var offsetX = (_currentTT == null ? _buttonPosition.Value.X : _buttonPosition.Value.X - _currentTT.X) + deltaX - mousePoint.X;
+                var offsetY = (_currentTT == null ? _buttonPosition.Value.Y : _buttonPosition.Value.Y - _currentTT.Y) + deltaY - mousePoint.Y;
+
+                (sender as Button).RenderTransform = new TranslateTransform(-offsetX, -offsetY);
+            };
             if (e.LeftButton == MouseButtonState.Pressed)
             {
+                
                 Task.Factory.StartNew(new Action(() =>
                 {
                     Thread.Sleep(100);
                     App.Current.Dispatcher.BeginInvoke(new Action(() =>
                     {
                         if (e.LeftButton == MouseButtonState.Pressed)
-                        {
+                        { 
+                            //GiveFeedbackEventHandler handler = new GiveFeedbackEventHandler(DragSource_GiveFeedback);
+                            //GiveFeedback += handler;
                             var data = new DataObject();
                             data.SetData("Source", (sender as Button));
                             DragDrop.DoDragDrop(sender as DependencyObject, data, DragDropEffects.Move);
+                            //GiveFeedback -= handler;
                             e.Handled = true;
                         }
                     }), null);
                 }), CancellationToken.None);
             }
+        }
+
+        void DragSource_GiveFeedback(object sender, GiveFeedbackEventArgs e)
+        {
+            try
+            {
+                //This loads the cursor from a stream .. 
+                //if (_allOpsCursor == null)
+                //{
+                //    using (Stream cursorStream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("SimplestDragDrop.DDIcon.cur"))
+                //    {
+                //        _allOpsCursor = new Cursor(cursorStream);
+                //    }
+                //}
+                //Mouse.SetCursor(customCursor);
+                e.UseDefaultCursors = false;
+                e.Handled = true;
+            }
+            finally { }
+        }
+
+        private void OnRendering(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (_buttonPosition == null)
+                _buttonPosition = (sender as Button).TransformToAncestor(blocklistview).Transform(new Point(0, 0));
+            var mousePosition = Mouse.GetPosition(blocklistview);
+            deltaX = mousePosition.X - _buttonPosition.Value.X;
+            deltaY = mousePosition.Y - _buttonPosition.Value.Y;
+            _isMoving = true;
+        }
+
+        private void PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            _currentTT = (sender as Button).RenderTransform as TranslateTransform;
+            _isMoving = false;
         }
     }
 
