37fbf95edeea2d6dcca4772337067b070b9bd7da
Tom <tom0917952503@gmail.com>
2023-04-05 00:48:25 +0800

    1. 將RadioButton改為ListView 2. 舊寫法以註解方式保留

--- ModernDesign/ModernDesign/MainViewModel.cs
+++ ModernDesign/ModernDesign/MainViewModel.cs
@@ -4,16 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZedGraph;
 
 namespace ModernDesign
 {
     class MainViewModel : ObservableObject
     {
 
-        public RelayCommand HomeViewCommand { get; set; }
-        public RelayCommand DiscoveryViewCommand { get; set; }
-
-        public RelayCommand SudokuViewCommand { get; set; }
+        //public RelayCommand HomeViewCommand { get; set; }
+        //public RelayCommand DiscoveryViewCommand { get; set; }
+        //public RelayCommand SudokuViewCommand { get; set; }
 
         private object _currentView;
         public object CurrentView 
@@ -43,6 +43,22 @@ namespace ModernDesign
             }
         }
 
+        private int selectIdx = 0;
+        public int SelectIdx
+        {
+            get
+            {
+                return selectIdx;
+            }
+            set
+            {
+                selectIdx = value;
+                SetCurrentView();
+                OnPropertyChanged();
+            }
+        }
+
+
         public MainViewModel()
         {
             var HomeVM = new HomeViewModel();
@@ -53,15 +69,32 @@ namespace ModernDesign
             var SudokuVM = new SudokuViewModel();
             SudokuView = new SudokuView(SudokuVM);
 
-            HomeViewCommand = new RelayCommand(o => { CurrentView = HomeView; });
-            DiscoveryViewCommand = new RelayCommand(o => { CurrentView = WebCamVM; });
-            SudokuViewCommand = new RelayCommand(o => { CurrentView = SudokuView; });
+            //HomeViewCommand = new RelayCommand(o => { CurrentView = HomeView; });
+            //DiscoveryViewCommand = new RelayCommand(o => { CurrentView = WebCamVM; });
+            //SudokuViewCommand = new RelayCommand(o => { CurrentView = SudokuView; });
 
             CurrentView = HomeView;
 
         }
 
-        
+        private void SetCurrentView()
+        {
+            switch (SelectIdx) 
+            {
+                case 0:
+                    CurrentView = HomeView;
+                    break;
+                case 1:
+                    CurrentView = WebCamVM;
+                    break;
+                case 2:
+                    CurrentView = SudokuView;
+                    break;
+            }
+        }
+
+
+
     }
 
 
